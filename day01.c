d86f2c58ffe78f8849cf51e7c1678381496c44ed
abhaypratapsingh1070-dot <abhaypratapsingh1070@gmail.com>
2025-08-24 14:41:36 +0530

    Add files via upload

---
+++ day01.c
@@ -0,0 +1,23 @@
+ // QUESTION 1 
+
+#include<stdio.h>
+
+int main() {
+ int num1, num2, sum;
+    // intput two numbers 
+    printf("enter the first number:");
+    scanf("%d", &num1);
+
+    printf("enter the second number:");
+    scanf("%d", &num2);
+
+    // calculate the sum
+    sum = num1 + num2;
+    
+    printf("the sum of %d and %d is %d\n" , num1 , num2 , sum);
+
+    return 0;
+} 
+    
+
+
