acaf1f41153e854e6a910fde2a75eac59e33ea1d
abhaypratapsingh1070-dot <abhaypratapsingh1070@gmail.com>
2025-08-26 22:25:03 +0530

    Add files via upload

--- 07.c
+++ 07.c
@@ -1,20 +1,20 @@
 //Write a program to calculate simple interest for given principal, rate, and time.
 # include<stdio.h>
 int main () {
-    int principal, rate, time, simpleInterest;
+    float principal, rate, time, simpleInterest;
 
    printf("enter principal:");
-   scanf("%d", &principal);
+   scanf("%f", &principal);
 
    printf("enter rate:");
-   scanf("%d", &rate);
+   scanf("%f", &rate);
 
    printf("enter time (in year):");
-   scanf("%d", &time);
+   scanf("%f", &time);
 
    simpleInterest = principal*rate*time/100;
 
-   printf("simpleInterest =%d", simpleInterest);
+   printf("simpleInterest =%.4f", simpleInterest);
 
    return 0;
 }
