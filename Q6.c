0d42ebc1373a69f6c579190aa31a89b853ded339
abhaypratapsingh1070-dot <abhaypratapsingh1070@gmail.com>
2025-09-08 14:51:31 +0530

    Add files via upload

---
+++ Q6.c
@@ -0,0 +1,23 @@
+//Calculate the remainder, and the quotient of two given numbers.
+
+#include <stdio.h>
+
+int main() {
+    int dividend, divisor, quotient, remainder;
+
+    
+    printf("Enter dividend: ");
+    scanf("%d", &dividend);
+
+    printf("Enter divisor: ");
+    scanf("%d", &divisor);
+
+    
+    quotient = dividend / divisor;
+    remainder = dividend % divisor;
+
+    printf("Quotient = %d\n", quotient);
+    printf("Remainder = %d\n", remainder);
+
+    return 0;
+}
